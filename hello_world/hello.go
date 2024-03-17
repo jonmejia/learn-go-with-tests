486c3565269692d2ffbf0cc1a296ea32ab5bffb5
jonmejia <jonathan@dataminded.net>
2024-03-17 19:08:07 -0400

    feat: now passing in hello as a const

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,9 +2,10 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix string = "Hello, "
 func Hello(name string) string {
 	//single quotes are for 'bytes' and 'runes' in golang, cannot be used for string
-	return "Hello, "+name
+	return englishHelloPrefix+name
 }
 
 func main() {
