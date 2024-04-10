c796e14ce07448277e0cd92f8237fbdb0ec1e361
jonmejia <jonathan@dataminded.net>
2024-04-10 11:13:07 -0400

    feat: added explicit typing to got and want variables and replaced q with s to indicate string in test error message

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Jonathan")
-	want := "Hello, Jonathan"
+	var got string = Hello("Jonathan")
+	var want string = "Hello, Jonathan"
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
