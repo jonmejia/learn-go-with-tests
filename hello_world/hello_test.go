e3ce57d1bbf611d52258e6d0b2aae76decd453ec
jonmejia <jonathan@dataminded.net>
2024-04-10 14:04:42 -0400

    test: added test case for when user does not enter name

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	var got string = Hello("Jonathan")
-	var want string = "Hello, Jonathan"
+	t.Run("test with param", func(t *testing.T) {
+		var got string = Hello("Jonathan")
+		var want string = "Hello, Jonathan"
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+
+	})
+	t.Run("test without param", func(t *testing.T) {
+		var got string = Hello("")
+		var want string = "Hello, World"
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+
+	})
 }
