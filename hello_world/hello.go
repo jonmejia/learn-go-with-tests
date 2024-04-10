df845056c1660909769ac19ffc238166d11663c0
jonmejia <jonathan@dataminded.net>
2024-04-10 11:13:21 -0400

    chore: remove comment + format

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 const englishHelloPrefix string = "Hello, "
+
 func Hello(name string) string {
-	//single quotes are for 'bytes' and 'runes' in golang, cannot be used for string
 	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix+name
+	return englishHelloPrefix + name
 }
 
 func main() {
