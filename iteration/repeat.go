8b16649c2c43f7f0c49861d3b35ee15b2718b4d1
jonmejia <jonathan@dataminded.net>
2024-04-11 10:31:43 -0400

    feat: updated function and test to add param so user can control how many times it loops

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
-func Repeat(input string) string {
+func Repeat(input string, repeatAmount int) string {
 	var output string 
-	for i := 0; i <4; i++ {
-		output = output + input 
+	for i := 0; i <repeatAmount; i++ {
+		output += input 
 	}
 	return output
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,7 +3,7 @@ package iteration
 import "testing"
 
 func TestIteration (t *testing.T){
-	var output string = Repeat("a") 
+	var output string = Repeat("a", 4) 
 	const expectedOutput = "aaaa"
 
 	if output != expectedOutput {
