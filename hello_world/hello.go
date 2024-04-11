9d0e8a0d8c47172d48b9636773fd815a7fa57025
jonmejia <jonathan@dataminded.net>
2024-04-11 09:11:23 -0400

    feat: refactored greeting function and added tests
    created test for spanish and french greeting

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -4,13 +4,21 @@ import "fmt"
 
 const englishHelloPrefix string = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return "Hola, " + name
+
+	}
+	if language == "French" {
+		return "Bonjour, " + name
+
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "Spanish"))
 }
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -10,14 +10,27 @@ func assertCorrectMessage(t testing.TB, got, want string) {
 }
 func TestHello(t *testing.T) {
 
-	t.Run("test with param", func(t *testing.T) {
-		var got string = Hello("Jonathan")
+	t.Run("test with English param", func(t *testing.T) {
+		var got string = Hello("Jonathan", "English")
 		var want string = "Hello, Jonathan"
 		assertCorrectMessage(t, got, want)
 
 	})
+	t.Run("test with Spanish param", func(t *testing.T) {
+		var got string = Hello("Jonathan", "Spanish")
+		var want string = "Hola, Jonathan"
+		assertCorrectMessage(t, got, want)
+
+	})
+	t.Run("test with French param", func(t *testing.T) {
+		var got string = Hello("Jonathan", "French")
+		var want string = "Bonjour, Jonathan"
+		assertCorrectMessage(t, got, want)
+
+	})
+
 	t.Run("test without param", func(t *testing.T) {
-		var got string = Hello("")
+		var got string = Hello("", "English")
 		var want string = "Hello, World"
 		assertCorrectMessage(t, got, want)
 
